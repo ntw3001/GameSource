2864240f1c183ed5b0735917cecbc467a4273900
Nick Westbrook <ntw3001@gmail.com>
2024-10-14 00:14:06 +0900

    imported toast and vuetify

--- src/main.js
+++ src/main.js
@@ -6,9 +6,27 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// toast
+import ToastPlugin from 'vue-toast-notification'
+import 'vue-toast-notification/dist/theme-bootstrap.css'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
+app.use(ToastPlugin)
 
 app.mount('#app')
