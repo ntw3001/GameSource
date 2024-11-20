7742f5636a9e37beb4e2d4131260ab3d1b259839
Nick Westbrook <ntw3001@gmail.com>
2024-11-21 01:21:38 +0900

    threw up that sweet articles list

--- src/components/user/dashboard/admin/articles.vue
+++ src/components/user/dashboard/admin/articles.vue
@@ -1,5 +1,50 @@
 <template>
-  looks deserted
+  <div class="text-center m-3" v-show="loading">
+    <v-progress-circular
+      indeterminate
+      color="primary"
+    />
+  </div>
+  <div v-show="!loading">
+    <v-table there="dark">
+      <thead>
+        <tr>
+          <th class="text-left">Name</th>
+          <th class="text-left">Owner</th>
+          <th class="text-left">Rating</th>
+          <th class="text-left">Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(article) in articleStore.adminArticles">
+          <td>{{ article.game }}</td>
+          <td>{{ article.owner.firstname }} {{ article.owner.lastname }}</td>
+          <td>{{ article.rating }}</td>
+          <td>{{ article.timestamp.toDate().toDateString() }}</td>
+          <td>
+            <v-btn
+              variant="outlined"
+              size="small"
+              color="green"
+              @click="router.push({name:'admin_edit', params:{id:article.id}})"
+            >
+              Ebit
+            </v-btn>
+          </td>
+          <td>
+            <v-btn
+              variant="outlined"
+              size="small"
+              color="red"
+            >
+              Deiete
+            </v-btn>
+          </td>
+
+        </tr>
+      </tbody>
+    </v-table>
+  </div>
 </template>
 
 <script setup>
@@ -7,16 +52,20 @@
 import { ref }  from 'vue'
 
 import { useArticleStore } from '@/stores/articles'
+import { useRoute, useRouter } from 'vue-router'
 const articleStore = useArticleStore()
 // import { useRouter } from 'vue-router'
 
 const loading = ref(false)
 const btnLoad = ref(false)
+const router = useRouter()
+const route = useRoute()
 
-loading.value;
-articleStore.adminGetArticles(3)
-.finally(() => {
-  loading.value = false
-})
-
+if(!articleStore.adminArticles || route.query.reload){
+  loading.value;
+  articleStore.adminGetArticles(3)
+  .finally(() => {
+    loading.value = false
+  })
+}
 </script>
