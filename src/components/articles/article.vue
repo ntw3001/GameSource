657e6456e452ae3deb0a638067227e0d4ada315f
Nick Westbrook <ntw3001@gmail.com>
2024-11-27 23:08:55 +0900

    Templated up the article pages

--- src/components/articles/article.vue
+++ src/components/articles/article.vue
@@ -1,3 +1,52 @@
 <template>
-  article
+  <div class="container p_top">
+    <div class="text-center m-3" v-if="loading">
+      <v-progress-circular
+        indeterminate
+        color="primary"
+      />
+    </div>
+    <div class="article_page" v-else>
+      <div class="game_tag">
+        <h1>{{ article.game }}</h1>
+      </div>
+      <div
+        class="article_featured"
+        :style='{backgroundImage:`url(${article.img})`}'
+      >
+      </div>
+      <div class="article_content">
+        <div class="owner">
+          Reviewed by <b>{{ article.owner.firstname }} {{ article.owner.lastname }}</b>
+        </div>
+        <hr/>
+        <h3>{{ article.title }}</h3>
+        <div class="editor" v-html="article.editor">
+        </div>
+        <hr/>
+        <div class="article_rating">
+          We give it a <b>{{ article.rating }}</b>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import { useArticleStore } from '@/stores/articles.js'
+const articleStore = useArticleStore()
+
+import { useRoute, } from 'vue-router'
+const route = useRoute()
+
+const loading = ref(true)
+const article = ref("")
+
+articleStore.getArticleById(route.params.id).then((data) => {
+  article.value = data
+}).finally(() => {
+  loading.value = false
+})
+
+</script>
